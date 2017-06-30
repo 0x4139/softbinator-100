3cadcbff5267d7a442cdc34fe462edbb4cf27d26
Vali Malinoiu <0x4139@gmail.com>
2017-06-30 16:00:26 +0300

    instructions for a vpn client certificate

--- infra/vpn.sh
+++ infra/vpn.sh
@@ -4,3 +4,8 @@
   docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u udp://VPN.infra.ink
   docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn ovpn_initpki
   docker run -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN --restart=always --network=internal kylemanna/openvpn
+
+
+
+  docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full INFRA.INK nopass
+  docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_getclient CLIENTNAME > INFRA_INK.ovpn
